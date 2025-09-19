780902e6a170a192e7b2c14be6e2c21d7aa9c593
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2025-09-19 15:31:40 +0600

    set up

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,29 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { createBrowserRouter, Route, Router, RouterProvider } from 'react-router'
+import { createRoutesFromElements } from 'react-router'
 
 function App() {
   const [count, setCount] = useState(0)
 
+  const myrout =createBrowserRouter(createRoutesFromElements(
+
+    <Route >
+
+      <Route index element={'/Home'} />
+
+    </Route>
+  ))
+
   return (
     <>
+
+    <RouterProvider router={myrout}/>
+    
       
     </>
+    
   )
 }
 
